896c7bdb5e288db0b915371aa3100df50c3fbb1f
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2024-01-14 18:21:30 +0800

    update UserRepository

--- src/main/java/com/example/weberpspringbackend/model/repository/UserRepository.java
+++ src/main/java/com/example/weberpspringbackend/model/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.weberpspringbackend.model.repository;
 import com.example.weberpspringbackend.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
