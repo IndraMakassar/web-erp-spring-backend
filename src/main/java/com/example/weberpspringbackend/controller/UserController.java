5b4232d6fdc099cbb76b7b0e691703f634a8aa87
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2024-01-14 18:22:11 +0800

    update user controller

--- src/main/java/com/example/weberpspringbackend/controller/UserController.java
+++ src/main/java/com/example/weberpspringbackend/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.weberpspringbackend.controller;
 
+import com.example.weberpspringbackend.config.AuthService;
+import com.example.weberpspringbackend.model.RegisterRequest;
 import com.example.weberpspringbackend.model.entity.User;
 import com.example.weberpspringbackend.model.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +13,9 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<Iterable<User>> getAllMeetings() {
@@ -25,14 +27,8 @@ public class UserController {
         }
     }
 
-    @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody User user) {
-        userRepository.save(user);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable long id) {
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
         Optional<User> user = userRepository.findById(id);
         return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
