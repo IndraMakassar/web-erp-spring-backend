6a918d45042686319713135ae4d04cd1ccc2ff87
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2024-01-14 22:36:01 +0800

    change how key were made and remove depricated

--- src/main/java/com/example/weberpspringbackend/config/JwtService.java
+++ src/main/java/com/example/weberpspringbackend/config/JwtService.java
@@ -2,10 +2,12 @@ package com.example.weberpspringbackend.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,9 +15,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
-    private static final String SECRET_KEY = "Ry9IAMnY3iosaWNpJh82O/tvCM7QTRmBF6q5r93Aq2WBiSkRbhvR43zkYkjcs9T7cr8+NvjT6diCXp+123+Q5PNePV5dv5owwIbo00bPjbdHOX0wOhnLf1YfXtxOGWKnzmPi3PU04q6I1JnRjVAGC5xA2aKRZdmhWIngQMJnhmQL4Kqg7Sp1F4pd+/O2PF8dccRL7P9lYIR62uurGrm9G3B32RNoDnq6G5YWo+XDvWfasbpEGmgiXF/1xOlEDERPMoTocYhRVTtkkaw8EvtfeDmqU8X4iSH8xp2CPHz4RH2veWc/XNu1pA/OxvTOgVSm6gFs7GbrU0y9lXmo4ijkHMNEQ+ifaeYxDebm7/6A33I=";
-    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 hours
+    private static final String SECRET_KEY = "dff16eaf00057cf973319c8982e157227b3c3abd408cb65fa1f5d07ba1afd9cf";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
     public static String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
@@ -24,11 +25,11 @@ public class JwtService {
 
     private static String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getKey())
                 .compact();
     }
 
@@ -56,9 +57,14 @@ public class JwtService {
 
     private static Claims extractAllClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+                .verifyWith(getKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private static SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
